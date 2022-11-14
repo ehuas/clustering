e91b7f371e291d1806a944ab2bafb5f922f6a5c8
Emily Huang <huange@dhcp-10-31-34-29.dyn.MIT.EDU>
2022-11-14 14:27:44 -0500

    clustering changes

--- clustering.py
+++ clustering.py
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 from statistics import mode
 from sklearn.preprocessing import StandardScaler
+import copy
 
 #%%
 
@@ -48,7 +49,9 @@ def load_data(path):
 
 from preProcessing import preProcessing, featExtract
 
-path = '/Volumes/common/scott/laminarPharm/mat/Lucky-DMS_ACSF_PFC-10162020-001.mat'
+#path = '/Volumes/common/scott/laminarPharm/mat/Lucky-DMS_ACSF_PFC-10162020-001.mat'
+#path = '/Volumes/common/scott/laminarPharm/mat/Lucky-DMS_ACSF_PFC-09192020-001.mat'
+path = '/Volumes/common/scott/laminarPharm/mat/Lucky-DMS_SALINE_PFC-07052021-001.mat'
 spike_times, spike_times_schema, unit_info, trial_info, session_info, spike_waves, spike_waves_schema = load_data(path)
 #%%
 def cluster_plots(cluster_stats, all_params):
@@ -59,31 +62,54 @@ def cluster_plots(cluster_stats, all_params):
     pass
 
 def GMM(features, num_reps):
-    components = np.arange(2, 10) # 2-9 clusters
+    components = np.arange(2, 24) # 2-9 clusters
     bics = np.zeros((num_reps, len(components)))
     aics = np.zeros((num_reps, len(components)))
+    min_comps = []
+    plt.figure(0)
     for rep in range(num_reps): # what is num_reps
+        min_bic = np.inf 
+        min_aic = np.inf
         for comp in components: # for each cluster #
             gmm = GaussianMixture(comp)
-            gmm.fit(features)
+            gmm_copy = copy.deepcopy(gmm)
+            gmm_copy.fit(features)
             
-            bic = gmm.bic(features)
-            aic = gmm.aic(features)
+            bic = gmm_copy.bic(features)
+            aic = gmm_copy.aic(features)
             
             bics[rep, comp-2] = bic
             aics[rep, comp-2] = aic
+            
+            if bic < min_bic:
+                min_bic = bic
+                min_comp = comp
+            if aic < min_aic: 
+                min_aic = aic
+        min_comps.append(min_comp)
+        plt.plot(components, bics[rep, :], label=str(rep), marker="o")
+        plt.title('all bics, 40 reps')
     
+    
+    plt.figure(1)
+    plt.boxplot(bics)
+    plt.figure(2)
+    plt.hist(min_comps)
+    average_min_comp = mode(min_comps)
+    print(min_comps)
+    print(average_min_comp)
     bics_mean = np.mean(bics, axis=0) #average per cluster (over reps)
     aics_mean = np.mean(aics, axis=0)
-    plt.figure(0)
-    plt.hist(components, bics_mean)
+    plt.figure(3)
+    plt.plot(components, bics_mean)
     plt.title('bics_mean')
     plt.xlabel('cluster #')
-    plt.figure(1)
-    plt.hist(aics_mean)
+    plt.figure(4)
+    plt.plot(components, aics_mean)
     plt.title('aics_mean')
     plt.xlabel('cluster #')
 # =============================================================================
+#    
 #     average_min_comp = mode(min_comps)
 #     gmm_min = GaussianMixture(average_min_comp)
 #     gmm_min.fit(features)
@@ -98,8 +124,10 @@ def main():
     all_params = ['meanRates', 'troughToPeak', 'repolTime', 'CV', 'LV']
     
     cluster_stats = featuresDF[all_params].to_numpy()
-    cluster_stats_new = np.zeros((51, 5))
-    for i in range(len(cluster_stats)):
+    num_params = len(all_params)
+    rows, = np.shape(featuresDF['meanRates'][0])
+    cluster_stats_new = np.zeros((rows, num_params))
+    for i in range(num_params):
         cluster_stats_new[:, i] = cluster_stats[0, i]
     scaler = StandardScaler() #do we need this
     cluster_stats_norm = scaler.fit_transform(cluster_stats_new)
