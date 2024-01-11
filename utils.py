009412d0a9e0dfce6567c5bb55e61d2abb230431
ehua <ehua@mit.edu>
2024-01-11 17:39:11 -0500

    added utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 15:22:24 2023
+
+@author: ehua
+"""
+import numpy as np
+
+def anscombe(x):
+    return 2.0*np.sqrt(x + 3.0/8.0)
